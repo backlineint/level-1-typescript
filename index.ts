cac6dbc6f78eee50e633467616859b1d63ddb3fe
Brian Perry <contact.brianperry@gmail.com>
2021-06-20 10:22:14 -0500

    3: basic types

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const isOpen: boolean = false;
+
+const myName: string = 'Scott';
+
+const myAge: number = 41;
+
+const list: number[] = [0,1,2];
+// Tuple
+const me: [string, number, boolean] = ['Brian', 41, false];
+
+enum Job { WebDev, WebDesigner, PM }
+const job: Job = Job.WebDev;
+
+const phone: any = 'iPhone';
+const tablet: any = 3;
+
+// Other types: null, undefined, never, object
